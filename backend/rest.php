b058ff1f9a7ef94208c76cc2a53f60c2625b36e8
Jason Reed <jrsofty@gmail.com>
2021-05-03 22:26:29 +0200

    Implements the rest api bootstrapper

---
+++ backend/rest.php
@@ -0,0 +1,18 @@
+<?php
+    header('Access-Control-Allow-Origin: *'); 
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
+    // Checks and defines constant to which prevents direct access.
+    if(!defined("CONST_KEY")){define("CONST_KEY", "035416f4-e65b-4fc6-a8db-301604ff31c5");}
+
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+
+    if(in_array($requestMethod, ["GET", "POST"])){
+
+    }else{
+        
+    }
+
+
+?>
