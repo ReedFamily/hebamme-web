76b486639f38939c6a237cd927cd08a8c45995a5
Jason Reed <jrsofty@gmail.com>
2021-05-03 22:26:03 +0200

    Creates the REST handler
    Provides redirect in the event directly browsed

---
+++ backend/api_handler.php
@@ -0,0 +1,8 @@
+<?php
+    if(!defined("CONST_KEY") || CONST_KEY !== "035416f4-e65b-4fc6-a8db-301604ff31c5"){
+        header("Location: ../404.html", true, 404);
+        echo file_get_contents('../404.html');
+	    die;
+    }
+
+?>
