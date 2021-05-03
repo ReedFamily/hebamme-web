7568d0750a0644ffb4b1d7a169eb59548f02ed28
Jason Reed <jrsofty@gmail.com>
2021-05-03 23:29:52 +0200

    Reduces methods to GET and POST
    Begins adding processing of request data

--- backend/rest.php
+++ backend/rest.php
@@ -1,6 +1,6 @@
 <?php
     header('Access-Control-Allow-Origin: *'); 
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Methods: GET, POST');
     header('Access-Control-Max-Age: 1000');
     header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
     // Checks and defines constant to which prevents direct access.
@@ -10,6 +10,12 @@
     require_once("./autoloader.php");
 
     if(in_array($requestMethod, ["GET", "POST"])){
+        $requestMethodArray = array();
+        $requestMethodArray = $_REQUEST;
+        if(isset($requestMethodArray["apiFunc"])){ $functionName = $requestMethodArray["apiFunc"];}
+        if(isset($requestMethodArray["apiParams"])){ $functionParams = $requestMethodArray["apiParams"];}
+
+
         echo(api_response::getResponse(200));
     }else{
         echo(api_response::getResponse(405));
