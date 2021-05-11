3911017af7c052532690dc835f949f8cd893bc0b
Jason Reed <jrsofty@gmail.com>
2021-05-11 20:19:03 +0200

    Updates database structure

--- backend/conf/sql/hebamme-web.sql
+++ backend/conf/sql/hebamme-web.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2021 at 09:11 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.11
+-- Generation Time: May 11, 2021 at 08:03 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.4.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -33,6 +32,39 @@ CREATE TABLE `api_tokens` (
   `valid_to` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `api_user`
+--
+
+CREATE TABLE `api_user` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `username` varchar(75) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `api_user`
+--
+
+INSERT INTO `api_user` (`id`, `username`, `password`, `role`) VALUES
+(1, 'Test1', 'asdfasfasfasfasdfasf', NULL),
+(2, 'Test2', 'agdagagasgasgahgsdfhdafh', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructors`
+--
+
+CREATE TABLE `instructors` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `team` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
@@ -42,6 +74,36 @@ CREATE TABLE `api_tokens` (
 --
 ALTER TABLE `api_tokens`
   ADD UNIQUE KEY `token` (`token`);
+
+--
+-- Indexes for table `api_user`
+--
+ALTER TABLE `api_user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_user` (`username`);
+
+--
+-- Indexes for table `instructors`
+--
+ALTER TABLE `instructors`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_name` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `api_user`
+--
+ALTER TABLE `api_user`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `instructors`
+--
+ALTER TABLE `instructors`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
