6077bedaef8e372282d867791544401d2d111b6d
Jason Reed <jrsofty@gmail.com>
2021-06-23 20:40:39 +0200

    Adds more to the instructors table

--- backend/conf/sql/hebamme-web.sql
+++ backend/conf/sql/hebamme-web.sql
@@ -41,7 +41,7 @@ CREATE TABLE `api_tokens` (
 --
 
 CREATE TABLE `api_user` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `username` varchar(75) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
@@ -59,17 +59,16 @@ CREATE TABLE `api_user` (
 --
 
 CREATE TABLE `instructor` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `position` varchar(128) DEFAULT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `last_name` varchar(128) NOT NULL,
   `first_name` varchar(128) NOT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `phone` varchar(128) DEFAULT NULL,
+  `mobile` varchar(128) DEFAULT NULL,
   `image_url` varchar(255) DEFAULT NULL,
-  `description` text DEFAULT NULL
+  `description` text DEFAULT NULL,
+  `position` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
 
 --
 -- Indexes for table `api_tokens`
@@ -81,32 +80,15 @@ ALTER TABLE `api_tokens`
 -- Indexes for table `api_user`
 --
 ALTER TABLE `api_user`
-  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `unique_user` (`username`);
 
 --
 -- Indexes for table `instructor`
 --
 ALTER TABLE `instructor`
-  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `instructors_name` (`last_name`,`first_name`) USING BTREE;
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `api_user`
---
-ALTER TABLE `api_user`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `instructor`
---
-ALTER TABLE `instructor`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
