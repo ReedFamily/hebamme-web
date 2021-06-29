7a3099cfc186bf29d2fe018891ef0e68d351f633
Jason Reed <jrsofty@gmail.com>
2021-06-29 22:40:15 +0200

    Implements instructors_processor to service

--- backend/instructors_processor.php
+++ backend/instructors_processor.php
@@ -1,30 +1,30 @@
 <?php
- if(!defined("CONST_KEY") || CONST_KEY !== "035416f4-e65b-4fc6-a8db-301604ff31c5"){
+    if(!defined("CONST_KEY") || CONST_KEY !== "035416f4-e65b-4fc6-a8db-301604ff31c5"){
         header("Location: ../404.html", true, 404);
         echo file_get_contents('../404.html');
         die;
     }
 
+    class instructors_processor{
+        public function createInstructor($params){
 
-    public function createInstructor($params){
+        }
+
+        public function updateInstructor($params){
+
+        }
+
+        public function listInstructors(){
+
+        }
+
+        public function getInstructor($params){
+
+        }
+
+        public function deleteInstructor($params){
+            
+        }
 
     }
-
-    public function updateInstructor($params){
-
-    }
-
-    public function listInstructors(){
-
-    }
-
-    public function getInstructor($params){
-
-    }
-
-    public function deleteInstructorById($params){
-        
-    }
-
-
 ?>
