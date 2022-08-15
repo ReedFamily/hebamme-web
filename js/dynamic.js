a26a7a0afdd9a60f600037959ad105087477d2ad
Jason Reed <jrsofty@gmail.com>
2022-08-15 20:23:01 +0200

    Updates frontend
    Implements changes for the alert message system
    cleans up some issues with the style and formatting
    Implements a Datenschutzerklaerung and Impressum

--- js/dynamic.js
+++ js/dynamic.js
@@ -20,23 +20,25 @@ const getTeam = function () {
 };
 
 const buildTeamCards = function (payload) {
-  var contentDiv = $("#team-content");
+  var contentDiv = $("#team-wrapper");
   jQuery.each(payload.instructors, function (index, instructor) {
+    var cardWrapper = jQuery("<div />", { class: "col-md-4, card-wrapper" });
     var card = jQuery("<div />", {
       class: "card",
       id: "team-member-" + instructor.id,
     });
-    var cardheader = jQuery("<div />", {
-      class: "card-header",
+    var cardheader = jQuery("<h4 />", {
+      class: "card-title",
       text: instructor.firstname + " " + instructor.lastname,
     });
     var img = jQuery("<img />", {
-      class: "card-img-top midwife-img",
+      class: "card-img-top",
       src: "./" + instructor.imageurl,
+      id: "team-member-img-" + instructor.id,
     });
     var cardbody = jQuery("<div />", { class: "card-body" });
-    var position = jQuery("<p />", {
-      class: "card-text",
+    var position = jQuery("<h5 />", {
+      class: "card-subtitle",
       text: instructor.position,
     });
     var descript = jQuery("<p />", {
@@ -44,12 +46,71 @@ const buildTeamCards = function (payload) {
       text: instructor.description,
     });
 
-    card.append(cardheader);
-    card.append(img);
-    card.append(cardbody);
+    cardbody.append(cardheader);
     cardbody.append(position);
     cardbody.append(descript);
 
-    contentDiv.append(card);
+    card.append(img);
+    card.append(cardbody);
+    cardWrapper.append(card);
+    contentDiv.append(cardWrapper);
   });
+};
+
+const getHomeAlerts = function () {
+  jQuery.get("backend/rest.php?apiFunc=getToken", function (res) {
+    response = JSON.parse(res);
+    if (response.status == 200) {
+      var apiToken = response.token;
+      var url =
+        "backend/rest.php?apiToken=" +
+        apiToken +
+        "&apiFunc=locMsgs&location=home";
+      jQuery.get(url, function (res1) {
+        response1 = JSON.parse(res1);
+        if (response1.status == 200) {
+          buildHomeAlerts(response1.messages);
+        } else {
+          console.log(response1);
+        }
+      });
+    } else {
+      console.log(response);
+    }
+  });
+};
+
+const buildHomeAlerts = function (payload) {
+  var msgSect = $("#home-alerts");
+  if (payload.length === 0) {
+    msgSect.toggleClass("hidden");
+  } else {
+    msgSect.toggleClass("hidden");
+    var highAlertsCont = $("#home-high-alerts");
+    var warnAlertsCont = $("#home-warn-alerts");
+    var infoAlertsCont = $("#home-info-alerts");
+    $.each(payload, function (index, msg) {
+      var alert = $("<div />", {
+        class: "alert",
+        role: "alert",
+        text: msg.message,
+      });
+      var clzz = "";
+      switch (msg.level) {
+        case "red":
+          clzz = "alert-danger";
+          highAlertsCont.append(alert);
+          break;
+        case "yellow":
+          clzz = "alert-warning";
+          warnAlertsCont.append(alert);
+          break;
+        default:
+          clzz = "alert-info";
+          infoAlertsCont.append(alert);
+          break;
+      }
+      alert.addClass(clzz);
+    });
+  }
 };
