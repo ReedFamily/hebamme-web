732a99534ce9f81f3c43d71c396346468d76ee08
Jason Reed <jrsofty@gmail.com>
2023-12-31 17:08:04 +0100

    Changes sitemap handling

--- dist.sh
+++ dist.sh
@@ -7,12 +7,14 @@ BUILD_NO=$2
 COMMIT=${GIT_COMMIT:0:7}
 VER=2.1.0
 COPYRIGHT_YEAR=2024
+LAST_MOD=$(date '+%Y-%m-%dT%H:%M:%S%z')
 DEPLOYNAME="../hebamme-web-deploy_${VER}_${COMMIT}_${BUILD_NO}.tar.gz"
 FLYWAYNAME="../hebamme-web-flyway_${VER}_${COMMIT}_${BUILD_NO}.tar.gz"
 
 echo "*** COPY FILES ***"
 
 cp ./index.html ./dist/upload/index.html
+cp ./sitemap.xml ./dist/upload/sitemap.xml
 cp ./LICENSE ./dist/upload/LICENSE
 cp ./hebamme_favicon128.ico ./dist/upload/hebamme_favicon128.ico
 cp ./favicon32.ico ./dist/upload/favicon32.ico
@@ -21,7 +23,8 @@ cp ./404.html ./dist/upload/404.html
 cp -R ./fonts ./dist/upload/fonts
 cp -R ./js ./dist/upload/js
 cp -R ./img ./dist/upload/img
-cp -R ./css ./dist/upload/css 
+cp -R ./css/*.css ./dist/upload/css 
+cp -R ./css/*.map ./dist/upload/css
 cp -R ./backend ./dist/upload/backend 
 cp -R ./admin ./dist/upload/admin
 
@@ -31,7 +34,8 @@ echo "*** PERFORM REPLACE ***"
 
 grep -rl GIT_HASH ./dist | xargs sed -i 's/GIT_HASH/'${BUILD_NO}'/'
 grep -rl REL_VER ./dist | xargs sed -i 's/REL_VER/'${VER}'/'
-grep -rl COPYRIGHT_YEAR | xargs sed -i 's/COPYRIGHT_YEAR/'${COPYRIGHT_YEAR}'/'
+grep -rl COPYRIGHT_YEAR ./dist | xargs sed -i 's/COPYRIGHT_YEAR/'${COPYRIGHT_YEAR}'/'
+grep -rl LAST_MOD ./dist | xargs sed -i 's/LAST_MOD/'${LAST_MOD}'/'
 
 echo "*** BUILD PACKAGE ***"
 
